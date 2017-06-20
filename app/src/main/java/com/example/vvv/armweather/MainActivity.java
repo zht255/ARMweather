421fe4d6f3b74fbd15b95a677a56cb68e184da16
vvv0307 <vvvzht@aliyun.com>
2017-06-20 09:40:57 +0800

    2017-06-20-2

--- app/src/main/java/com/example/vvv/armweather/MainActivity.java
+++ app/src/main/java/com/example/vvv/armweather/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.vvv.armweather;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void initActionBar(){
+        Toolbar toolbar = (Toolbar)findViewById(R.id.my_toolbar);
+        toolbar.setTitle("ARM weather");
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.about);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+
     }
 }
